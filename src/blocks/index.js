67a4b4ae7bb9e144ea66c0032895eef26695d366
shershnev.danil2000@gmail.com <shershnev.danil2000@gmail.com>
2020-06-02 10:53:08 +0300

    save

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -2,6 +2,9 @@ import './logo';
 import './nav';
 import './task';
 import './btn';
+import './section';
+import './filter';
+import './video';
 
 export default () => {
   console.log('init blocks');
