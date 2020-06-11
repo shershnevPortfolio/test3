ddddc02dcda914f29cf047a91156043c386bd38f
shershnev.danil2000@gmail.com <shershnev.danil2000@gmail.com>
2020-06-11 16:23:29 +0300

    save

--- src/blocks/form/index.js
+++ src/blocks/form/index.js
@@ -3,6 +3,7 @@ const $input = $block.find('.file-upload');
 const getView = data => {
   return `
     <div class='file-label'>
+      <div class='file-label__button'></div>
       <div class='file-label__text'>
         <span class='file-label__title'>
           ${data.name}
@@ -16,18 +17,48 @@ const getView = data => {
     `
 }
 
+const toggleInputState = ($input) => {
+    const defaultState = `
+      <label class="file-label" for="upload-file">
+      <div class="file-label__button">
+        <img src="../../images/staple.svg" alt=""/></div><div class="file-label__text">
+        <span class="file-label__title">Прикрепить файлы</span>
+        <span class="file-label__requires">Jpg, png, gif, mp4 не более 15 Мб</span>
+      </div>
+      <input class="file-upload" type="file" id="upload-file" multiple="multiple" accept=".png, ,jpg, .mp4, .gif"/>
+    </label>
+    `
+
+    const uploadMore = `
+      <label class="file-label file-label--more" for="upload-file">
+      <div class="file-label__button">
+        <img src="../../images/plus.svg" alt=""/></div><div class="file-label__text">
+        <span class="file-label__title">Загрузить еще</span>
+      </div>
+      <input class="file-upload" type="file" id="upload-file" multiple="multiple" accept=".png, ,jpg, .mp4, .gif"/>
+    </label>
+
+    `
+
+    $input.parent('.file-label').html(uploadMore);
+}
+
+
+const reverse = () => $block.find('.form__file-component').toggleClass('form__file-component--reversed');
+
 export default () => {
   $input.on('change', function() {
   const files = $(this).prop('files');
   const viewsList = [];
-  console.log(files)
     for(let key in files) {
         if(key !== 'length' && key !== 'item') {
           const fileData = files[key];
           viewsList.push(getView(fileData))
         }
     }
-    $block.find('.form__file-list').append(viewsList)
-    console.log(viewsList);
+
+    reverse();
+    $block.find('.form__file-list').append(viewsList);
+    toggleInputState($(this));
   })
 }
