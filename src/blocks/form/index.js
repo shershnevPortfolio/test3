9a9852a022149af7477605fd10eb32e96101106a
shershnev.danil2000@gmail.com <shershnev.danil2000@gmail.com>
2020-06-19 09:46:07 +0300

    save p1

--- src/blocks/form/index.js
+++ src/blocks/form/index.js
@@ -68,25 +68,45 @@ const modifyData = (obj, key) => {
 
 }
 
+
+const imitLoad = ($item, loaded) => {
+  const loadingView =   `<div class='loading'>
+    <div style='width: ${loaded * 25}%' class='loading__progress-bar'></div>
+  </div>`
+  if(loaded < 4) {
+    $item.html(loadingView);
+  }
+}
+
 export default () => {
   $input.on('change', function() {
     console.log('changed');
   const files = $(this).prop('files');
   console.log(files);
   const viewsList = [];
+  let i = 0;
     for(let key in files) {
         if(key !== 'length' && key !== 'item') {
           const fileData = files[key];
           const newData = modifyData(fileData, key);
           viewsList.push(getView(newData));
+          i++;
+          setInterval(() => {
+            i++;
+            imitLoad($block.find('.form__file-list'), i);
+          }, 500)
         }
     }
 
     reverse();
-    $block.find('.form__file-list').append(viewsList);
-    $('.file-label__cross').on('click', function() {
-      const $fileLabel = $($(this).parents('.file-label')[0]).remove();
+    setTimeout(() => {
+      $block.find('.form__file-list').append(viewsList);
+      clearInterval(loadedInterval);
+      $('.loading').remove();
+    }, 2000 )
 
+    $('.file-label__cross').on('click', function() {
+      $($(this).parents('.file-label')[0]).remove();
     });
     toggleInputState($(this));
   })
